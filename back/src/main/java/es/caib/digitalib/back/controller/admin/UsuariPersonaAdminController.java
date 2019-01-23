411525cccfb01e2a65b1944b262a09af1c0351d0
scalafat-dgtic <scalafat@dgtic.caib.es>
2019-01-23 11:34:18 +0100

    Actualitzar Login durant un Canvi d'informacio d'usuari #31

--- back/src/main/java/es/caib/digitalib/back/controller/admin/UsuariPersonaAdminController.java
+++ back/src/main/java/es/caib/digitalib/back/controller/admin/UsuariPersonaAdminController.java
@@ -1,19 +1,28 @@
 package es.caib.digitalib.back.controller.admin;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
 
 import org.fundaciobit.genapp.common.StringKeyValue;
 import org.fundaciobit.genapp.common.i18n.I18NException;
+import org.fundaciobit.genapp.common.i18n.I18NValidationException;
 import org.fundaciobit.genapp.common.query.Field;
 import org.fundaciobit.genapp.common.query.SelectMultipleStringKeyValue;
 import org.fundaciobit.genapp.common.web.form.AdditionalField;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -22,9 +31,14 @@ import org.springframework.web.servlet.ModelAndView;
 import es.caib.digitalib.back.controller.webdb.UsuariPersonaController;
 import es.caib.digitalib.back.form.webdb.UsuariPersonaFilterForm;
 import es.caib.digitalib.back.form.webdb.UsuariPersonaForm;
+import es.caib.digitalib.back.security.LoginInfo;
+import es.caib.digitalib.jpa.ConfiguracioGrupJPA;
+import es.caib.digitalib.jpa.UsuariPersonaJPA;
+import es.caib.digitalib.logic.ConfiguracioGrupLogicaLocal;
 import es.caib.digitalib.model.entity.UsuariPersona;
 import es.caib.digitalib.model.fields.UsuariPersonaFields;
 import es.caib.digitalib.model.fields.UsuariPersonaQueryPath;
+import es.caib.digitalib.utils.Constants;
 
 /**
  * 
@@ -37,9 +51,11 @@ import es.caib.digitalib.model.fields.UsuariPersonaQueryPath;
 public class UsuariPersonaAdminController extends UsuariPersonaController{
 
 	public static final String CONTEXTWEB = "/admin/usuariPersona";
-	public static final String CONTEXTWEBCONFGRUP = "/admin/configuracioGrup/";
 
 	public static final int CONFIGURACIOGRUPCOLUMN= 1;
+	
+	@EJB(mappedName = ConfiguracioGrupLogicaLocal.JNDI_NAME)
+  protected ConfiguracioGrupLogicaLocal configuracioGrupLogicaEjb;
 
 	@Override
 	public String getTileForm() {
@@ -91,31 +107,75 @@ public class UsuariPersonaAdminController extends UsuariPersonaController{
 		return usuariPersonaFilterForm;
 	}
 	
-//	XYZ ZZZ --> Que els canvis de header i footer s'actualitzin al moment
-//	@Override
-//	public UsuariPersonaJPA update(HttpServletRequest request, UsuariPersonaJPA usuariPersona)
-//		    throws Exception,I18NException, I18NValidationException {
-//		
-//			super.usuariPersonaEjb.update(usuariPersona);
-//			LoginInfo logInf = LoginInfo.getInstance();
-//			UsuariPersonaJPA up = logInf.getUsuariPersona();
-//			
-//			if (up.getUsername() != usuariPersona.getUsername()) {
-//				up.setUsername(usuariPersona.getUsername());
-//			} else if (up.getNif() != usuariPersona.getNif()) {
-//				
-//			} else if (up.getNom() != usuariPersona.getNom()) {
-//				
-//			} else if (up.getLlinatges() != usuariPersona.getLlinatges()) {
-//				
-//			} else if (up.getEmail() != usuariPersona.getEmail()) {
-//				
-//			} else if (up.getIdioma() != usuariPersona.getIdioma()) {
-//				
-//			}
-//			return up;    
-//		    
-//		  }
+	@Override
+	public UsuariPersonaJPA update(HttpServletRequest request, UsuariPersonaJPA usuariPersona)
+		    throws Exception,I18NException, I18NValidationException {
+		
+			updateLoginInfo(usuariPersona);
+			return super.update(request, usuariPersona);   
+		    
+	}
+	
+	private void updateLoginInfo(UsuariPersonaJPA usuariPersona) {
+	  //Si hi ha canvis entre les noves dades de UsuariPersona i el LoginInfo, s'actualitza LoginInfo
+	  SecurityContext sc = SecurityContextHolder.getContext();
+    Authentication au = sc.getAuthentication();
+	  
+	  User user = (User) au.getPrincipal();
+	  
+	  Set<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+	  Collection<GrantedAuthority> seyconAuthorities = user.getAuthorities();
+	  
+	  roles.addAll(seyconAuthorities);
+	  
+	  LoginInfo logInf = LoginInfo.getInstance();
+    UsuariPersonaJPA up = logInf.getUsuariPersona();
+    
+    //Dades normals
+    if (!up.getUsername().equals(usuariPersona.getUsername())) {
+      up.setUsername(usuariPersona.getUsername());
+    } 
+    if (!up.getNif().equals(usuariPersona.getNif())) {
+      up.setNif(usuariPersona.getNif());
+    } 
+    if (!up.getNom().equals(usuariPersona.getNom())) {
+      up.setUsername(usuariPersona.getNom());
+    } 
+    if (!up.getLlinatges().equals(usuariPersona.getLlinatges())) {
+      up.setLlinatges(usuariPersona.getLlinatges());
+    } 
+    if (!up.getEmail().equals(usuariPersona.getEmail())) {
+      up.setEmail(usuariPersona.getEmail());
+    } 
+    if (!up.getIdiomaID().equals(usuariPersona.getIdiomaID())) {
+      up.setIdiomaID(usuariPersona.getIdiomaID());
+    } 
+     
+    //Permisos
+    if (up.isRoleCoAu() != usuariPersona.isRoleCoAu()) 
+      up.setRoleCoAu(usuariPersona.isRoleCoAu());
+    if (up.isRoleCust() != usuariPersona.isRoleCust()) 
+      up.setRoleCust(usuariPersona.isRoleCust());
+    if (up.isRoleScan() != usuariPersona.isRoleScan()) 
+      up.setRoleScan(usuariPersona.isRoleCust());
+    
+    if (up.isRoleScan())
+      roles.add(new SimpleGrantedAuthority(Constants.ROLE_SCAN));
+    if (up.isRoleCoAu())
+      roles.add(new SimpleGrantedAuthority(Constants.ROLE_COAU));
+    if (up.isRoleCust())
+      roles.add(new SimpleGrantedAuthority(Constants.ROLE_CUST));
+    
+    //Configuracio Grup
+    if (up.getConfiguracioGrupID() != usuariPersona.getConfiguracioGrupID()) {
+      ConfiguracioGrupJPA configuracioGrup = configuracioGrupLogicaEjb.findByPrimaryKey(usuariPersona.getConfiguracioGrupID());
+      up.setConfiguracioGrup(configuracioGrup);
+    }
+    boolean necesitaConfigurar = false;
+    LoginInfo loginInfo = new LoginInfo(user, up, roles, necesitaConfigurar);
+    
+    SecurityContextHolder.getContext().setAuthentication(loginInfo.generateToken());
+	}
 	
 
 	@Override
@@ -142,7 +202,7 @@ public class UsuariPersonaAdminController extends UsuariPersonaController{
 			for (StringKeyValue confgrup : confgrups) {
 
 				str.append("<a href=\""
-						+ request.getContextPath() + CONTEXTWEBCONFGRUP + up.getConfiguracioGrupID()
+						+ request.getContextPath() + ConfiguracioGrupAdminController.CONTEXTWEB + up.getConfiguracioGrupID()
 						+ "/edit\">" + confgrup.getValue() + "</a><br/>\n");
 
 				map.put(key, str.toString());
