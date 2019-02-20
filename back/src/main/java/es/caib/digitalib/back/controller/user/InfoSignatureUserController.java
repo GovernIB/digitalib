73a1d693242977bf6feedfaa536cbe43a2702f23
scalafat-dgtic <scalafat@dgtic.caib.es>
2019-02-20 13:51:25 +0100

    Correcció d'errors de navegació #82

--- back/src/main/java/es/caib/digitalib/back/controller/user/InfoSignatureUserController.java
+++ back/src/main/java/es/caib/digitalib/back/controller/user/InfoSignatureUserController.java
@@ -1,12 +1,18 @@
 package es.caib.digitalib.back.controller.user;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import es.caib.digitalib.back.controller.AbstractInfoSignatureController;
 import es.caib.digitalib.back.form.webdb.InfoSignaturaFilterForm;
 import es.caib.digitalib.back.form.webdb.InfoSignaturaForm;
+import es.caib.digitalib.back.utils.Utils;
+import es.caib.digitalib.jpa.InfoSignaturaJPA;
 
 /**
  * 
@@ -24,6 +30,20 @@ public class InfoSignatureUserController extends AbstractInfoSignatureController
   @Override
   public boolean isAdmin() {
     return false;
+  }
+  
+  @Override
+  public InfoSignaturaForm getInfoSignaturaForm(InfoSignaturaJPA _jpa, boolean __isView,
+      HttpServletRequest request, ModelAndView mav) throws I18NException {
+    InfoSignaturaForm infoSignaturaForm = super.getInfoSignaturaForm(_jpa, __isView, request,
+        mav);
+
+    Utils.hideNullFields(infoSignaturaForm.getInfoSignatura(), infoSignaturaForm,
+        ALL_INFOSIGNATURA_FIELDS);
+    infoSignaturaForm.setCancelButtonVisible(false);
+
+    return infoSignaturaForm;
+
   }
 
 }
