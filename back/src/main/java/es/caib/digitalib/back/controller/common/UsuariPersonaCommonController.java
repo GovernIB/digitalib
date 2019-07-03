6e871f824d7be844560eec32780dd9532d9e7cf7
scalafat-dgtic <scalafat@dgtic.caib.es>
2019-07-03 12:21:36 +0200

    Bug: Falta de refresc de LoginInfo #92

--- back/src/main/java/es/caib/digitalib/back/controller/common/UsuariPersonaCommonController.java
+++ back/src/main/java/es/caib/digitalib/back/controller/common/UsuariPersonaCommonController.java
@@ -1,9 +1,21 @@
 package es.caib.digitalib.back.controller.common;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
 
 import org.fundaciobit.genapp.common.i18n.I18NException;
+import org.fundaciobit.genapp.common.i18n.I18NValidationException;
 import org.fundaciobit.genapp.common.web.i18n.I18NUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,9 +23,12 @@ import org.springframework.web.servlet.ModelAndView;
 import es.caib.digitalib.back.controller.webdb.UsuariPersonaController;
 import es.caib.digitalib.back.form.webdb.UsuariPersonaForm;
 import es.caib.digitalib.back.security.LoginInfo;
+import es.caib.digitalib.jpa.ConfiguracioGrupJPA;
 import es.caib.digitalib.jpa.UsuariPersonaJPA;
+import es.caib.digitalib.logic.ConfiguracioGrupLogicaLocal;
 import es.caib.digitalib.model.entity.UsuariPersona;
 import es.caib.digitalib.utils.Configuracio;
+import es.caib.digitalib.utils.Constants;
 
 /**
  * 
@@ -24,6 +39,9 @@ import es.caib.digitalib.utils.Configuracio;
 @RequestMapping(value = "/common/usuaripersona")
 public class UsuariPersonaCommonController extends UsuariPersonaController {
 
+  @EJB(mappedName = ConfiguracioGrupLogicaLocal.JNDI_NAME)
+  protected ConfiguracioGrupLogicaLocal configuracioGrupLogicaEjb;
+  
   @Override
   public String getTileForm() {
     return "usuariPersonaFormCommon";
@@ -73,6 +91,74 @@ public class UsuariPersonaCommonController extends UsuariPersonaController {
     return usuariPersonaForm;
   }
 
+  @Override
+  public UsuariPersonaJPA update(HttpServletRequest request, UsuariPersonaJPA usuariPersona) throws Exception, I18NException, I18NValidationException {
+	  updateLoginInfo(usuariPersona);
+	  return super.update(request, usuariPersona);  
+  }
+  
+  private void updateLoginInfo(UsuariPersonaJPA usuariPersona) {
+	  //Si hi ha canvis entre les noves dades de UsuariPersona i el LoginInfo, s'actualitza LoginInfo
+	SecurityContext sc = SecurityContextHolder.getContext();
+    Authentication au = sc.getAuthentication();
+	  
+	User user = (User) au.getPrincipal();
+	  
+	Set<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+	Collection<GrantedAuthority> seyconAuthorities = user.getAuthorities();
+	  
+	roles.addAll(seyconAuthorities);
+	  
+    LoginInfo logInf = LoginInfo.getInstance();
+    UsuariPersonaJPA up = logInf.getUsuariPersona();
+    
+    //Dades normals
+    if (up.getUsername().equals(usuariPersona.getUsername())) {
+      
+      if (!up.getNif().equals(usuariPersona.getNif())) {
+        up.setNif(usuariPersona.getNif());
+      } 
+      if (!up.getNom().equals(usuariPersona.getNom())) {
+        up.setNom(usuariPersona.getNom());
+      }
+      if (!up.getLlinatges().equals(usuariPersona.getLlinatges())) {
+        up.setLlinatges(usuariPersona.getLlinatges());
+      } 
+      if (!up.getEmail().equals(usuariPersona.getEmail())) {
+        up.setEmail(usuariPersona.getEmail());
+      } 
+      if (!up.getIdiomaID().equals(usuariPersona.getIdiomaID())) {
+        up.setIdiomaID(usuariPersona.getIdiomaID());
+      } 
+     
+      //Permisos
+      if (up.isRoleCoAu() != usuariPersona.isRoleCoAu()) 
+        up.setRoleCoAu(usuariPersona.isRoleCoAu());
+      if (up.isRoleCust() != usuariPersona.isRoleCust()) 
+        up.setRoleCust(usuariPersona.isRoleCust());
+      if (up.isRoleScan() != usuariPersona.isRoleScan()) 
+        up.setRoleScan(usuariPersona.isRoleCust());
+      
+      if (up.isRoleScan())
+        roles.add(new SimpleGrantedAuthority(Constants.ROLE_SCAN));
+      if (up.isRoleCoAu())
+        roles.add(new SimpleGrantedAuthority(Constants.ROLE_COAU));
+      if (up.isRoleCust())
+        roles.add(new SimpleGrantedAuthority(Constants.ROLE_CUST));
+    
+      //Configuracio Grup
+      if (up.getConfiguracioGrupID() != usuariPersona.getConfiguracioGrupID()) {
+        ConfiguracioGrupJPA configuracioGrup = configuracioGrupLogicaEjb.findByPrimaryKey(usuariPersona.getConfiguracioGrupID());
+        up.setConfiguracioGrup(configuracioGrup);
+      }
+      
+      boolean necesitaConfigurar = false;
+      LoginInfo loginInfo = new LoginInfo(user, up, roles, necesitaConfigurar);
+      
+      SecurityContextHolder.getContext().setAuthentication(loginInfo.generateToken());
+    }
+  }
+  
   @Override
   public String getRedirectWhenModified(HttpServletRequest request,
       UsuariPersonaForm usuariPersonaForm, Throwable __e) {
