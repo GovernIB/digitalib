74b88f8cc0d3965938a698aa054e07b1bd24797b
anadal-fundaciobit <anadal@fundaciobit.org>
2019-08-21 09:55:16 +0200

    Aplicar millores de API de Firma i de GenApp #93

---
+++ compile.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo optional parameters -Dcaib -Psqlgen
+
+cmd /C mvn -DskipTests %* install
+
+if %errorlevel% EQU 0 (
+
+	@echo off
+	IF DEFINED DIGITALIB_DEPLOY_DIR (
+      for /f "tokens=* delims=" %%x in (versio.txt) do set DIGITALIB_VERSIO=%%x
+	  @echo on
+	  echo --------- COPIANT EAR %DIGITALIB_VERSIO% ---------
+
+	  xcopy /Y ear\target\digitalib.ear %DIGITALIB_DEPLOY_DIR%
+
+	) ELSE (
+	  echo  =================================================================
+	  echo    Definex la variable d'entorn DIGITALIB_DEPLOY_DIR apuntant al
+	  echo    directori de deploy del JBOSS  i automaticament s'hi copiara
+	  echo    l'ear generat.
+	  echo  =================================================================
+	) 
+
+)
