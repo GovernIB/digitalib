b8ff71208e0d652d6ef65617e29b470eb3e549cd
digitalib <digitalib@digitalib.fundaciobit.org>
2019-11-07 10:23:16 +0100

    Retornar les dades omplides al formulari en REST #96

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo off
-cat help.txt
+#cat help.txt
 
 env mvn -DskipTests $@ install 
 
@@ -13,9 +13,9 @@ if [ $? == 0 ]; then
     echo    directori de deploy del JBOSS  i automaticament s\'hi copiara
     echo    l\'ear generat.
     echo  =================================================================
-  
+
   else
-  
+
     echo on
     echo --------- COPIANT EAR ---------
     cp ./ear/target/digitalib.ear $DIGITALIB_DEPLOY_DIR
