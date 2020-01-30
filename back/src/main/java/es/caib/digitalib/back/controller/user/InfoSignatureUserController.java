077e06c3a12c6921a7b3ffc90d9182033deafa2f
scalafat-dgtic <scalafat@dgtic.caib.es>
2020-01-30 09:05:39 +0100

    Millora de la navegació en la informació de la Transacció de pipella usuari + ocultació informació no rellevant per usuari #101

--- back/src/main/java/es/caib/digitalib/back/controller/user/InfoSignatureUserController.java
+++ back/src/main/java/es/caib/digitalib/back/controller/user/InfoSignatureUserController.java
@@ -1,9 +1,14 @@
 package es.caib.digitalib.back.controller.user;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +18,9 @@ import es.caib.digitalib.back.form.webdb.InfoSignaturaFilterForm;
 import es.caib.digitalib.back.form.webdb.InfoSignaturaForm;
 import es.caib.digitalib.back.utils.Utils;
 import es.caib.digitalib.jpa.InfoSignaturaJPA;
+import es.caib.digitalib.logic.TransaccioLogicaLocal;
+import es.caib.digitalib.model.entity.Transaccio;
+import es.caib.digitalib.model.fields.TransaccioFields;
 
 /**
  * 
@@ -27,9 +35,31 @@ public class InfoSignatureUserController extends AbstractInfoSignatureController
 
   public static final String CONTEXT_WEB_USER_INFO_SIGNATURE = "/user/infoSignatura";
 
+  @EJB(mappedName = TransaccioLogicaLocal.JNDI_NAME)
+  protected TransaccioLogicaLocal transaccioLogicaEjb;
+  
   @Override
   public boolean isAdmin() {
     return false;
+  }
+  
+  @Override
+  public String cancelInfoSignatura(@PathVariable("infoSignaturaID") java.lang.Long infoSignaturaID,
+	      HttpServletRequest request,HttpServletResponse response) {
+	     
+	Long id = 0L;
+	try {
+		List<Transaccio> list = transaccioLogicaEjb.select(TransaccioFields.INFOSIGNATURAID.equal(infoSignaturaID));
+		if (list == null || list.size() == 0) {
+	      return null;
+	    }
+
+		id = list.get(0).getTransaccioID();
+		
+	} catch (I18NException e) {
+		e.printStackTrace();
+	}
+	  return "redirect:/user/transaccio/custodia/view/"+ id;
   }
   
   @Override
@@ -40,7 +70,7 @@ public class InfoSignatureUserController extends AbstractInfoSignatureController
 
     Utils.hideNullFields(infoSignaturaForm.getInfoSignatura(), infoSignaturaForm,
         ALL_INFOSIGNATURA_FIELDS);
-    infoSignaturaForm.setCancelButtonVisible(false);
+    infoSignaturaForm.setCancelButtonVisible(true);
 
     return infoSignaturaForm;
 
