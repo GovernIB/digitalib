23b339684950d19daf74c5f69f21dcc31ee04281
anadal-fundaciobit <anadal@fundaciobit.org>
2020-09-09 10:24:15 +0200

    Obrir Versió #115

--- model/src/main/java/es/caib/digitalib/versio/Versio.java
+++ model/src/main/java/es/caib/digitalib/versio/Versio.java
@@ -3,5 +3,5 @@ package es.caib.digitalib.versio;
 * Codi autogenerat a partir del fitxer Versio.java.template.
 */
 public interface Versio {
-public static final String VERSIO="1.0.4";
+public static final String VERSIO="1.0.5";
 }
