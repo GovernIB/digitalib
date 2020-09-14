eec370fc81646ea934bd19ce8295b14335b33b12
anadal-fundaciobit <anadal@fundaciobit.org>
2020-09-14 08:09:06 +0200

    Errors en script de BBDD

--- scripts/bbdd/1.0/oracle/digitalib_update_from_1.0.1_to_1.0.2.sql
+++ scripts/bbdd/1.0/oracle/digitalib_update_from_1.0.1_to_1.0.2.sql
@@ -2,20 +2,20 @@
 -- 2020/04/07 
 
 ALTER TABLE dib_transaccio
-  ADD COLUMN infoscanpapersize varchar2(100);
+  ADD infoscanpapersize varchar2(100);
   
 ALTER TABLE dib_transaccio
-  ADD COLUMN infoscandatacaptura timestamp;
+  ADD infoscandatacaptura timestamp;
   
   
 CREATE TABLE dib_metadada
 (
-   metadadaid number(19,0) NOT NULL DEFAULT dib_digitalib_seq.nextval, 
+   metadadaid number(19,0)  DEFAULT dib_digitalib_seq.nextval NOT NULL, 
    transaccioid number(19,0) NOT NULL, 
    nom varchar2(255) NOT NULL,
    valor varchar2(3000) NOT NULL, 
    CONSTRAINT dib_metadada_pk PRIMARY KEY (metadadaid), 
-   CONSTRAINT dib_meta_transaccio_trans_fk FOREIGN KEY (transaccioid) REFERENCES dib_transaccio (transaccioid) ON UPDATE NO ACTION ON DELETE NO ACTION
+   CONSTRAINT dib_meta_transaccio_trans_fk FOREIGN KEY (transaccioid) REFERENCES dib_transaccio (transaccioid)
 );
 
 
