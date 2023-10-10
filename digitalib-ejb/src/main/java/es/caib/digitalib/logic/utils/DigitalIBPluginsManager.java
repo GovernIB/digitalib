e6d7069dff5e34cd00731c7d13f1bfebc98a7689
Antoni Nadal <anadal@fundaciobit.org>
2023-10-10 08:59:51 +0200

    Migració de DigitalIB 1.0 (Jboss 5.2) a JBoss 7.2 #205

--- digitalib-ejb/src/main/java/es/caib/digitalib/logic/utils/DigitalIBPluginsManager.java
+++ digitalib-ejb/src/main/java/es/caib/digitalib/logic/utils/DigitalIBPluginsManager.java
@@ -6,9 +6,8 @@ import org.fundaciobit.pluginsib.core.utils.PluginsManager;
 import org.fundaciobit.genapp.common.i18n.I18NArgumentCode;
 import org.fundaciobit.genapp.common.i18n.I18NException;
 
+import es.caib.digitalib.commons.utils.Configuracio;
 import es.caib.digitalib.commons.utils.Constants;
-
-
 
 /**
  * 
@@ -17,27 +16,24 @@ import es.caib.digitalib.commons.utils.Constants;
  */
 public class DigitalIBPluginsManager implements Constants {
 
-  protected static Logger log = Logger.getLogger(DigitalIBPluginsManager.class);
+    protected static Logger log = Logger.getLogger(DigitalIBPluginsManager.class);
 
-  public static final String LOGIN_PLUGIN_KEY = DIGITALIB_PROPERTY_BASE + "userinformationplugin";
+    public static final String LOGIN_PLUGIN_KEY = DIGITALIB_PROPERTY_BASE + "userinformationplugin";
 
-  public static IUserInformationPlugin loginPlugin = null;
-  
+    public static IUserInformationPlugin loginPlugin = null;
 
-
-  public static IUserInformationPlugin getUserInformationPluginInstance() throws I18NException {
-    if (loginPlugin == null) {
-      final String propertyPlugin = LOGIN_PLUGIN_KEY;
-      Object pluginInstance = PluginsManager.instancePluginByProperty(propertyPlugin, Constants.DIGITALIB_PROPERTY_BASE);
-      if (pluginInstance == null) {
-        // XYZ ZZZ Cercar traduccions a PortaFIB
-        throw new I18NException("plugin.donotinstantiateplugin", new I18NArgumentCode("plugin.userinfo"));
-      }
-      loginPlugin = (IUserInformationPlugin) pluginInstance;
+    public static IUserInformationPlugin getUserInformationPluginInstance() throws I18NException {
+        if (loginPlugin == null) {
+            final String propertyPlugin = LOGIN_PLUGIN_KEY;
+            Object pluginInstance = PluginsManager.instancePluginByProperty(propertyPlugin,
+                    Constants.DIGITALIB_PROPERTY_BASE, Configuracio.getSystemAndFileProperties());
+            if (pluginInstance == null) {
+                // XYZ ZZZ Cercar traduccions a PortaFIB
+                throw new I18NException("plugin.donotinstantiateplugin", new I18NArgumentCode("plugin.userinfo"));
+            }
+            loginPlugin = (IUserInformationPlugin) pluginInstance;
+        }
+        return loginPlugin;
     }
-    return loginPlugin;
-  }
-
-
 
 }
